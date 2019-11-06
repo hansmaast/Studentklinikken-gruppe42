99e13ba1925265e9f577d3ff2d0477edcee2bf35
Hans Maast <hfmaast@gmail.com>
2019-11-06 12:13:31 +0100

    Add inputFields

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,18 +1,54 @@
 <template>
-    <div>
-        <h2>Login</h2>
-        <login-or-register />
+    <div class="mainContainer">
+        <h1>Logg inn</h1>
+        <st-input-icon
+            label="E-postadresse"
+            placeholder="example@mail.com"
+            imgSrc="#"
+        />
+        <st-input-icon
+                label="Passord"
+                placeholder="**********"
+                imgSrc="#"
+        />
+        <p>Glemt passord?</p>
+        <div class="imgContainer"></div>
     </div>
 </template>
 
 <script>
-    import LoginOrRegister from "@/components/Forms/LoginOrRegister";
+    import StInputIcon from "../components/InputFields/StInputIcon";
 
     export default {
         name: 'Login',
-        components: {LoginOrRegister},
+        components: {StInputIcon},
         data() {
             return {}
         }
     }
 </script>
+
+<style>
+    .mainContainer {
+        height: 100vh;
+        width: 100vw;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .imgContainer {
+        border: 1px solid;
+        max-width: 400px;
+        max-height: 400px;
+        width: 75vw;
+        height: 75vw;
+    }
+
+    p {
+        margin: 0px auto;
+        max-width: 1000px;
+        font-size: 14px;
+    }
+</style>
