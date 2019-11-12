d22dd71a07dd094a2caffb901c7d832ed2307bbf
Hans Maast <hfmaast@gmail.com>
2019-11-12 13:52:16 +0100

    Add Register component

---
+++ src/components/Forms/StRegister.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="loginContainer">
+        <h1>Registerer deg</h1>
+        <div class="fields">
+            <st-input-email
+                    label="E-postadresse"
+                    type="email"
+                    placeholder="Skriv inn din e-post.."
+            />
+            <st-input-user
+                    label="Navn"
+                    type="text"
+                    placeholder="Skriv inn ditt navn.."
+            />
+            <st-input-password
+                    label="Passord"
+                    type="password"
+                    placeholder="Skriv inn ditt passord.."
+            />
+            <st-input-password
+                    label="Passord"
+                    type="password"
+                    placeholder="Gjenta passord.."
+            />
+        </div>
+        <div class="loginFoot">
+            <router-link to="login">
+                <p>Allerede bruker?</p>
+            </router-link>
+            <st-small-btn
+                    label="FULLFØR"
+            />
+        </div>
+    </div>
+</template>
+<script>
+    import StInputEmail from "../InputFields/StInputEmail";
+    import StInputUser from "../InputFields/StInputUser";
+    import StInputPassword from "../InputFields/StInputPassword"
+    import StSmallBtn from "../Buttons/StSmallBtn";
+
+    export default {
+        name: 'st-register',
+        components: {StInputPassword, StInputEmail, StInputUser, StSmallBtn}
+    }
+</script>
+<style scoped>
+    a {
+        outline: none;
+        text-decoration: none;
+        color: inherit;
+    }
+    .loginContainer {
+        display: grid;
+        grid-template-areas: "a" "b" "b" "b" "c";
+        margin-bottom: 15px;
+    }
+
+    h1 {
+        grid-area: a;
+    }
+
+    .fields {
+        grid-area: b;
+    }
+
+    .loginFoot {
+        grid-area: c;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-evenly;
+    }
+</style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,18 +1,38 @@
 <template>
-    <div>
-        <h2>Register</h2>
-        <BookingForm />
-    </div>
-</template>
+    <div class="mainContainer">
+        <st-register/>
+    </div></template>
 
 <script>
-    import BookingForm from "@/components/Forms/BookingForm";
-
+    import StRegister from "../components/Forms/StRegister";
     export default {
         name: 'Register',
-        components: {BookingForm},
+        components: {StRegister},
         data() {
             return {}
         }
     }
 </script>
+
+<style scoped>
+    .mainContainer {
+        height: 100vh;
+        width: 100vw;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .imgContainer {
+        max-width: 400px;
+        max-height: 400px;
+        width: 75vw;
+        height: 75vw;
+    }
+
+    .imgContainer > img {
+        width: 100%;
+        height: 100%;
+    }
+</style>
