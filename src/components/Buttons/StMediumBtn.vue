c01a2221d3bbf5753a4eb12e278852f028a4ebf7
Hans Maast <hfmaast@gmail.com>
2019-11-12 15:08:21 +0100

    Add treatmentFields && mediumBtn to Home

---
+++ src/components/Buttons/StMediumBtn.vue
@@ -0,0 +1,30 @@
+<template>
+    <button>{{label}}</button>
+</template>
+
+<script>
+    export default {
+        name: 'st-medium-btn',
+        props: {
+            label: String
+        }
+    }
+</script>
+
+<style scoped>
+    button {
+        position: relative;
+        width: 90vw;
+        height: 90vh;
+        font-size: 14px;
+        font-weight: bold;
+        max-width: 110pt;
+        max-height: 39pt;
+        text-align: center;
+        background-color: #77999E;
+        border-radius: 50px;
+        border: none;
+        outline: none;
+        color: white;
+    }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="mainContainer">
+        <h1>Studentklinikken</h1>
+        <p>Hei! Vi tilbyr følgende:</p>
+        <div class="treatments">
+            <st-treatment-field treatment="Akupunktur"/>
+            <st-treatment-field treatment="Kostholdsveiledning"/>
+            <st-treatment-field treatment="Osteopati"/>
+            <st-treatment-field treatment="Fysiologisk testlab"/>
+        </div>
+        <st-medium-btn label="BESTILL TIME"/>
+        <st-quote-field/>
+    </div>
+</template>
+
+<script>
+
+    // Tells the component what to export
+    import StTreatmentField from "../components/Fields/StTreatmentField";
+    import StMediumBtn from "../components/Buttons/StMediumBtn";
+    import StQuoteField from "@/views/StQuoteField";
+
+    export default {
+        name: 'Home',
+        components: {
+            StQuoteField,
+            StTreatmentField,
+            StMediumBtn
+            // Always declare the component you want to render
+        },
+        data() {
+            // The userData object is bound to the v-model.. Needs some more knowlege about this
+            return {
+                textFromInput: 'Write something'
+            }
+        },
+        methods: {
+            // This is the components method functions, it is basically functions inside an object
+            alertMessage() {
+                // Using "this" to refer to the components own properties?
+                alert(this.textFromInput)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .mainContainer {
+        height: 100vh;
+        width: 100vw;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: center;
+    }
+
+    .treatments {
+        margin-bottom: 10%;
+    }
+
+    .treatmentField > p {
+        font-size: 18px;
+        margin-left: 2pt;
+    }
+
+    .iconContainer > img {
+        width: 100%;
+        height: 100%;
+    }
+</style>
