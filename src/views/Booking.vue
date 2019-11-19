5c13d606304ddd716e17b0d35f113337ad0cc4eb
Hans Maast <hfmaast@gmail.com>
2019-11-19 18:53:15 +0100

    Refactor booking page

--- src/views/Booking.vue
+++ src/views/Booking.vue
@@ -2,7 +2,21 @@
     <div class="mainContainer">
         <h1>Studentklinikken</h1>
         <p class="ingress">Bestill time hos <br> studentklinikken</p>
-        <st-booking-selector :price="price" :treatment="treatment"/>
+<!--        <st-booking-selector :price="price" :treatment="treatment"/>-->
+        <div class="selectFields">
+            <select v-model="treatment" name="treatment" id="selectTreatment">
+                <option value="Akupunktur">Akupunktur</option>
+                <option value="Kostholdsveiledning">Kostholdsveiledning</option>
+                <option value="Osteopati">Osteopati</option>
+                <option value="Fysiologisk testlab">Fysiologisk testlab</option>
+            </select>
+            <select v-model="price" name="prisKlasse" id="selectPrice">
+                <option value="200">Voksen</option>
+                <option value="150">Student</option>
+                <option value="150">Honnør</option>
+                <option value="150">Ansatt</option>
+            </select>
+        </div>
         <h4>Velg dato:</h4>
         <date-picker
                 class="calendar"
@@ -12,15 +26,21 @@
         />
         <h4>Velg klokkeslett:</h4>
         <div id="hourPicker">
-        <st-hour-picker id ="hourPicker" :hours="hours"/>
+<!--        <st-hour-picker id ="hourPicker" :hours="hours" :appointment="appointment"/>-->
+            <div class="timeContainer">
+                <div @click="addColor" v-for="hour in hours" class="timeBox">
+                    {{hour}}
+                </div>
+            </div>
         </div>
         <div id="textareaBooking">
         <textarea placeholder="Legg til kommentar her...">
         </textarea>
         </div>
-        <p>Pris: {{this.price}}</p>
         <p>Behandling: {{this.treatment}}</p>
         <p>Dato: {{this.date.toLocaleDateString()}}</p>
+        <p>Klokkeslett: {{this.time}}</p>
+        <p>Pris: {{this.price}}</p>
         <router-link to="mybookings">
             <st-medium-btn @click="submitBooking" label="BESTILL"/>
         </router-link>
@@ -30,16 +50,12 @@
 <script>
     // Tells the component what to export
     import DatePicker from 'v-calendar/lib/components/date-picker.umd'
-    import StHourPicker from "@/views/StHourPicker";
     import StMediumBtn from "../components/Buttons/StMediumBtn";
-    import StBookingSelector from "@/views/StBookingSelector";
     import userData from "../userData/userData";
 
     export default {
         name: 'Home',
         components: {
-            StBookingSelector,
-            StHourPicker,
             DatePicker,
             StMediumBtn
             // Always declare the component you want to render
@@ -49,9 +65,10 @@
             return {
                 // TODO: Link this to local storage?
                 treatment: "Akupunktur",
-                price: parseInt("200"),
                 date: new Date(),
-                hours: ["08.00", "09.00", "10.00", "11.00", "12.00", "13.00", "14.00", "15.00", "16.00", "17.00", "18.00"],
+                time: "08:00",
+                price: parseInt("200"),
+                hours: ["08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00"],
                 user: userData
             }
         },
@@ -65,6 +82,18 @@
                     time: '12.30',
                     address: 'Waldemars Kjøkken 66'
                 });
+            },
+            addColor(e) {
+                let element = document.getElementsByClassName("timeBox");
+                let color = "rgb(119, 153, 158)";
+                for (var i = 0; i < element.length; i++) {
+                    if (element[i].style.backgroundColor = color) {
+                        element[i].style.backgroundColor = "";
+                    }
+                }
+                console.log(e.target.style.backgroundColor);
+                e.target.style.backgroundColor != color ? e.target.style.backgroundColor = color : e.target.style.backgroundColor = "";
+                this.time = e.target.innerText;
             }
         }
     }
@@ -72,14 +101,64 @@
 
 <style scoped>
 
-    #hourPicker, #textareaBooking {
-        display: -webkit-box;
-        border: none;
+
+    .selectFields {
+        width: 85vw;
+        max-width: 400px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .selectFields > select {
+        width: 100%;
+        margin-bottom: 15px;
+    }
+
+    select {
+        border: 0;
+        outline: none;
+        background-color: inherit;
+        border-bottom: 1px solid;
+        border-radius: 0;
+        appearance: none; /* Remove default arrow */
+        background-image: url(../../public/icon/arrowDown.svg); /* Add custom arrow */
+        background-repeat: no-repeat;
+        background-position: right;
+        font-size: 18px;
+        padding: 0 5px 5px 5px;
     }
 
     .calendar {
         width: 85vw;
         max-width: 400px;
+    }
+
+    .timeContainer {
+        width: 85vw;
+        max-width: 400px;
+        padding: 10px 0;
+        border: none;
+        overflow-x: scroll;
+        display: -webkit-box;
+        margin-bottom: 0px;
+    }
+
+    .timeContainer::-webkit-scrollbar {
+        display: none;
+    }
+
+    .timeBox {
+        width: 50px;
+        height: 36px;
+        border: 1px solid #9d9d9d;
+        margin: 0 7.5px;
+        border-radius: 6px;
+        font-size: 14px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        transition: all 0.2s ease;
     }
 
     * {
@@ -94,7 +173,7 @@
     }
 
     textarea {
-        width: 85vw;
+        width: 75vw;
         max-width: 400px;
         height: 100px;
         outline: none;
@@ -102,9 +181,12 @@
         font-size: 1em;
         padding: 0.7em;
         border-radius: 4px;
-        border: 1px solid rgba(53, 73, 94, 0.3);
-        box-shadow: 0px 5px 10px rgba(53, 73, 94, 0.2);
+        border: 1px solid var(--main-gray);
         transition: all 0.3s;
+    }
+
+    textarea::placeholder {
+        color: var(--main-gray);
     }
 
     p {
