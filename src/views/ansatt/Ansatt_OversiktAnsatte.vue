f46720b1e1b94703e5fd43239491e7913d690478
Hans Maast <hfmaast@gmail.com>
2019-11-22 20:33:01 +0100

    Implement employeeCard in OversiktAnsatte page

--- src/views/ansatt/Ansatt_OversiktAnsatte.vue
+++ src/views/ansatt/Ansatt_OversiktAnsatte.vue
@@ -1,31 +1,86 @@
 <template>
-  <div class="mainContainer">
-    <h1>Ansattoversikt</h1>
-  </div>
+    <div class="mainContainer">
+        <div class="contentContainer">
+            <h1>Ansatte</h1>
+            <div class="infoCardContainer">
+              <employeeCard
+                      v-for="ansatt in this.ansattData"
+                      :name="ansatt.name"
+                      :profession="ansatt.profession"
+                      :phone-nr="ansatt.phoneNr"
+                      :email="ansatt.email"
+              />
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
-  name: "ansattoversikt",
-  components: {},
-  data() {
-    return {};
-  }
-};
+  import EmployeeCard from "@/views/ansatt/EmployeeCard";
+  import { ansattData } from "../../userData/ansattData";
+
+  export default {
+        name: "Login",
+        components: {EmployeeCard},
+        data() {
+            return {
+              ansattData: ansattData
+            };
+        }
+    };
 </script>
 
 <style scoped>
-/*
-        Ferdig med pirkings fra prototype
-    */
+    h3,
+    h5,
+    p {
+        margin: 10px 0 0 0;
+        font-size: 18px;
+        font-weight: bold;
+    }
 
-.mainContainer {
-  display: flex;
-  margin-top: 166px;
-}
+    h3 {
+        font-size: 21px;
+    }
 
-.imgContainer > img {
-  width: 100%;
-  height: 100%;
-}
+    h5 {
+        font-weight: normal;
+    }
+
+    .contentContainer {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        padding: 40px 80px;
+        box-sizing: border-box;
+    }
+
+    .infoCardContainer {
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+      flex-wrap: wrap;
+        align-items: center;
+        justify-content: flex-start;
+        padding: 20px 0;
+    }
+
+    .ansattImg > img {
+        width: 100%;
+        height: 100%;
+    }
+
+    @media (max-width: 1100px) {
+        .infoCardContainer {
+            flex-direction: column;
+          flex-wrap: nowrap;
+          justify-content: center;
+        }
+
+        .contentContainer {
+            align-items: center;
+        }
+    }
 </style>
---
+++ src/views/ansatt/EmployeeCard.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="employeeCard">
+        <div class="ansattInfo">
+            <div class="ansattImg">
+                <img src="#" alt="">
+            </div>
+            <div class="infoContainer">
+                <h3>{{this.name}}</h3>
+                <h5>{{this.profession}}</h5>
+            </div>
+            <div class="infoContainer">
+                <p>{{this.phoneNr}}</p>
+                <p>{{this.email}}</p>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'employeeCard',
+        props: {
+            name: String,
+            profession: String,
+            phoneNr: String,
+            email: String
+        }
+    }
+</script>
+<style scoped>
+    h3,
+    h5,
+    p {
+        margin: 10px 0 0 0;
+        font-size: 18px;
+        font-weight: bold;
+    }
+
+    h3 {
+        font-size: 21px;
+    }
+
+    h5 {
+        font-weight: normal;
+    }
+
+    .employeeCard {
+        width: 100%;
+        height: 100%;
+        max-width: 300px;
+        max-height: 350px;
+        margin: 20px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: center;
+        box-sizing: border-box;
+        border: 1px solid #ebeff2;
+        border-radius: 9px;
+        background-color: #FFF;
+    }
+
+    .ansattInfo {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: center;
+        padding: 10px;
+        box-sizing: border-box;
+    }
+
+    .ansattImg {
+        border: 1px solid;
+        border-radius: 50%;
+        height: 50vw;
+        width: 50vw;
+        max-width: 155px;
+        max-height: 155px;
+    }
+
+    .infoContainer {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .ansattImg > img {
+        width: 100%;
+        height: 100%;
+    }
+
+    @media (max-width: 1070px) {
+
+    }
+</style>
